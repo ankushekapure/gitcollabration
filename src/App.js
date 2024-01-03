941de5120d0c38a7826beb6edf60d343d5c000fe
Ankush Ekapure <asekapure@gmail.com>
2024-01-03 13:02:06 +0530

    First Box Design

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>This Is First Time Change</h1>
+      <h1 style={{ width: "200px", height: "200px", backgroundColor: "green", padding: "10px", margin: "50px" }}>
+        This Is First Time Change
+      </h1>
     </div>
   );
 }
