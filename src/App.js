a2d75261a3cc3743acce65676c6d354a6b30cd7a
Ankush Ekapure <asekapure@gmail.com>
2024-01-03 13:09:25 +0530

    Added Paragraph

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <h1>
         This Is First Time Change
       </h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio veritatis necessitatibus commodi maxime rem saepe deleniti accusamus, facilis nesciunt repellat! Maiores, eos blanditiis quam asperiores nobis, magnam in eius saepe fuga velit, culpa omnis. Id nostrum aliquam quam?</p>
     </div>
   );
 }
